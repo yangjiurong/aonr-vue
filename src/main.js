ed3c9b43a7d8cf0a8b8777189fe6d409a4120469
yangjiurong <47538607@qq.com>
2017-11-30 22:00:12 +0800

    Test ssh key connection

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import 'vue-awesome/dist/vue-awesome'
 
 Vue.use(ElementUI)
 
